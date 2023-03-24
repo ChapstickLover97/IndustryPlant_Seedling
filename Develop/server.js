edb36b1b6593324ccde2f6c1e942f2af955c7d9f
Dr. Taintlove <sirwilliamwood@gmail.com>
2023-03-23 21:33:45 -0500

    re added structure

--- Develop/server.js
+++ Develop/server.js
@@ -1,7 +1,8 @@
 const express = require('express');
 const session = require('express-session');
+const exphbs = require('express-handlebars');
 const routes = require('./controllers');
-
+const helpers = require('./utils/helpers');
 const sequelize = require('./config/connection');
 const SequelizeStore = require('connect-session-sequelize')(session.Store);
 
@@ -17,6 +18,14 @@ const sess = {
     db: sequelize
   })
 };
+
+// Create the Handlebars.js engine object with custom helper functions
+const hbs = exphbs.create({ helpers });
+
+// Inform Express.js which template engine we're using
+app.engine('handlebars', hbs.engine);
+app.set('view engine', 'handlebars');
+
 
 app.use(session(sess));
 

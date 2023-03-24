7c9a7534feb9c0d445717787ea157e555bd42825
MUNTAAS <98139114+Mhassan0630@users.noreply.github.com>
2023-03-24 08:11:59 -0500

    stillworkingonit

---
+++ controllers/homeRoutes.js
@@ -0,0 +1,12 @@
+const router = require('express').Router();
+router.get('/', async (req, res) => { 
+    try {
+       res.render('landing') 
+    } catch (error) {
+        res.status(500).json(error);
+
+        
+    }
+});
+
+module.exports = router;

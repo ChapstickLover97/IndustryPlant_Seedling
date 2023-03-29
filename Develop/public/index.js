541cb7cdaf3ec3ac76c9b65dfcf5bbbef94c03e7
Ashton Balder <AshtonBalder@Ashs-MacBook-Pro.local>
2023-03-28 19:13:42 -0500

    others

--- Develop/public/index.js
+++ Develop/public/index.js
@@ -23,3 +23,21 @@ showSignupStuff.addEventListener("click", function() {
     passwordSignup.style.display = "block";
     submitBtn.style.display = "block";
 })
+
+function myFunction() {
+    document.getElementById("myDropdown").classList.toggle("show");
+  }
+  
+  // Close the dropdown menu if the user clicks outside of it
+  window.onclick = function(event) {
+    if (!event.target.matches('.dropbtn')) {
+      var dropdowns = document.getElementsByClassName("dropdown-content");
+      var i;
+      for (i = 0; i < dropdowns.length; i++) {
+        var openDropdown = dropdowns[i];
+        if (openDropdown.classList.contains('show')) {
+          openDropdown.classList.remove('show');
+        }
+      }
+    }
+  }
